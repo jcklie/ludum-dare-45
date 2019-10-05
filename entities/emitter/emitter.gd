5ccadc9b5f68d25eae105d1e572dfa4e014ad0c7
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-10-05 17:05:13 +0200

    Add emitter with raycast.

--- entities/emitter/emitter.gd
+++ entities/emitter/emitter.gd
@@ -1,23 +1,32 @@
 extends StaticBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-var note = "a";
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var MAX_RAYCAST_LENGTH = 2000
+var ray_target;
 
 func rotate_left():
 	self.rotation_degrees -= 90;
 	
 func rotate_right():
 	self.rotation_degrees += 90;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var m = Transform2D().rotated(global_rotation)
+	var emitter_direction = m.xform(Vector2(0,-1))	# emitters currently point upwards by default	
+	
+	var world_phys_state = get_world_2d().direct_space_state
+	var ray_end_pos = global_position + emitter_direction * MAX_RAYCAST_LENGTH
+	var hit = world_phys_state.intersect_ray(global_position, ray_end_pos, [self], 5)	# impassable terrain, emitter, drain
+	
+	ray_target = to_local(hit["position"]) if "position" in hit else null
+	update()
+	
+	self.rotation_degrees += 1
+
+func _draw():
+	if ray_target:
+		draw_line(Vector2(0,0), ray_target, Color(255, 0, 0), 5)
