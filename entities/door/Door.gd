4c60d312d4bcffc658be13df892e45d2cebc5037
Jan-Christoph Klie <git@mrklie.com>
2019-10-05 18:55:51 +0200

    goal

--- entities/door/Door.gd
+++ entities/door/Door.gd
@@ -9,15 +9,16 @@ func _ready():
 	sprite.connect("animation_finished", self, "_on_animation_finished")
 	
 func open():
-	if not open:
+	if not open and not sprite.playing:
 		open = true
 		sprite.play("open")
 
 func close():
-	if open:
+	if open and not sprite.playing:
 		open = false
 		sprite.play("close")
 
 	
 func _on_animation_finished():
 	collider.disabled = open
+	sprite.playing = false
--- entities/goal/Goal.gd
+++ entities/goal/Goal.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,12 +9,5 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	for node in get_children():
-		print(node);
-       # if(node.is_type(type)): 
-        #    count += 1
-	
-	
-	pass
+#func _process(delta):
+#	pass
