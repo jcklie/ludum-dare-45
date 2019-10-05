449efc41ec32ac1b6022742de6f0723f7631753c
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-10-05 14:47:36 +0200

    Add emitter, dummy level code

---
+++ entities/emitter/emitter.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var note = "a";
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func rotate_left():
+	self.rotation_degrees -= 90;
+	
+func rotate_right():
+	self.rotation_degrees += 90;
