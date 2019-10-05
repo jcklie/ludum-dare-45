ac8de85f8550af6f11ce468f8fc9b35df48c422b
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-10-05 19:56:18 +0200

    First round of sound effects stuff. Beam sound doesn't work as intended.

--- entities/door/Door.gd
+++ entities/door/Door.gd
@@ -2,6 +2,9 @@ extends StaticBody2D
 
 onready var collider = $"CollisionShape2D"
 onready var sprite = $"AnimatedSprite"
+onready var asp = $AudioStreamPlayer
+onready var sfx_open = preload("res://audio/sfx/door_open.wav")
+onready var sfx_close = preload("res://audio/sfx/door_close.wav")
 
 var open = false
 
@@ -12,13 +15,17 @@ func open():
 	if not open and not sprite.playing:
 		open = true
 		sprite.play("open")
+		asp.stream = sfx_open
+		asp.play()
 
 func close():
 	if open and not sprite.playing:
 		open = false
 		sprite.play("close")
+		asp.stream = sfx_close
+		asp.play()
 
-	
 func _on_animation_finished():
 	collider.disabled = open
 	sprite.playing = false
+	asp.stop()
