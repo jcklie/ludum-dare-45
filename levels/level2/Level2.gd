292bf336f374a5f0cad82b66559205ec06dbadb6
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-10-06 15:51:01 +0200

    Drain is now called ReceiverSingle

--- levels/level2/Level2.gd
+++ levels/level2/Level2.gd
@@ -4,7 +4,7 @@ onready var drain1 = $"drains/Drain1"
 onready var drain4 = $"drains/Drain4"
 onready var door = $Door
 
-func _process(delta):	
+func _process(delta):
 	if 69 in drain1.inputs and 76 in drain4.inputs:
 		door.open()
 	else:
