3e71e958e87f312d0da74bfd558e591e4a12f49a
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-10-06 13:30:07 +0200

    Pull out drain attenuation stuff into new AudioStreamPlayer

---
+++ entities/PlayerAttenuatingAudioStreamPlayer.gd
@@ -0,0 +1,30 @@
+extends AudioStreamPlayer
+
+class_name PlayerAttenuatingAudioStreamPlayer
+
+var _base_volume_db = 0
+var _max_hearable_distance = 150
+var _attenuation = 1
+var _call = 0
+
+func set_params(base_volume_db, max_hearable_distance, attenuation):
+	self._base_volume_db = base_volume_db
+	self._max_hearable_distance = max_hearable_distance
+	self._attenuation = attenuation
+
+func _process(delta):
+	# don't update volume levels every call
+	if _call % Globals.AUDIO_UPDATE_FREQ == 0:
+		# taking some inspiration from audio_stream_player_2d.cpp
+		var own_position = get_parent().global_position
+		var distance_to_player = own_position.distance_to(Globals.player.global_position)
+		if distance_to_player > self._max_hearable_distance:
+			stop()
+		else:
+			if not playing:
+				play()
+			var multiplier = 1.0 - pow(distance_to_player / self._max_hearable_distance, self._attenuation)
+			multiplier *= db2linear(self._base_volume_db)
+			volume_db = linear2db(multiplier)
+
+	_call = (_call + 1) % Globals.AUDIO_UPDATE_FREQ
--- entities/drain/Drain.gd
+++ entities/drain/Drain.gd
@@ -5,28 +5,10 @@ extends StaticBody2D
 export var midi_note_number : int = 69
 onready var note = preload("res://logic/note.gd").Note.new(midi_note_number)
 
-onready var asp = $AudioStreamPlayer
-
-const BASE_VOLUME_DB = 0
-const MAX_HEARABLE_DISTANCE = 150
-const ATTENUATION = 1
+onready var asp = $PlayerAttenuatingAudioStreamPlayer
 
 var inputs = {}
-var call = 0
 
 func _ready():
+	asp.set_params(0.0, 150.0, 1.0)
 	asp.pitch_scale = note.well_tempered_factor_from_concert_pitch()
-
-func _process(delta):	
-	if call % Globals.AUDIO_UPDATE_FREQ == 0:
-		# taking some inspiration from audio_stream_player_2d.cpp
-		var distance_to_player = global_position.distance_to(Globals.player.global_position)
-		if distance_to_player > MAX_HEARABLE_DISTANCE:
-			asp.stop()
-		else:
-			if not asp.playing:
-				asp.play()
-			var multiplier = 1.0 - pow(distance_to_player / MAX_HEARABLE_DISTANCE, ATTENUATION)
-			multiplier *= db2linear(BASE_VOLUME_DB)
-			asp.volume_db = linear2db(multiplier)
-	call = (call + 1) % Globals.AUDIO_UPDATE_FREQ
