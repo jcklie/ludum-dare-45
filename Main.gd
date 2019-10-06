bb56b2b6e8809060a625feecdee4de517e59a3c4
Jan-Christoph Klie <git@mrklie.com>
2019-10-06 15:42:47 +0200

    Level can be goaled

--- Main.gd
+++ Main.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
+const LEVEL_NAME = "current_level"
+var level_number = 1
+var level
+
 func _ready():
-	var level_id = "1"
+	_spawn_level(1)
+	
+func _spawn_level(level_id):
+	if level:
+		remove_child(level)
+	
 	var level_name = "res://levels/level{id}/Level{id}.tscn"
 	var level_scene = load(level_name.format({"id": level_id}))	
-	var level = level_scene.instance()
+	level = level_scene.instance()
 	
 	var goal = level.get_node("Goal")
 	goal.connect("body_entered", self, "_on_reach_goal")
@@ -14,4 +23,5 @@ func _ready():
 func _on_reach_goal(body):
 	if body.get_name() == "Player":
 		print("Reached goal")
-
+		level_number += 1
+		_spawn_level(level_number)
