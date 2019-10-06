c12200697a485fa831646ba6315a63f054c8547a
Jan-Christoph Klie <git@mrklie.com>
2019-10-06 17:42:04 +0200

    Add error state

--- levels/level3/Level3.gd
+++ levels/level3/Level3.gd
@@ -4,6 +4,7 @@ onready var rec_mul = $"receivers/ReceiverMulti"
 onready var goal = $Goal
 
 func _process(delta):
+
 	var success = rec_mul.inputs.size() == 2 and 69 in rec_mul.inputs and 76 in rec_mul.inputs
 	if success:
 		goal.set_enabled(true)
@@ -12,4 +13,5 @@ func _process(delta):
 		goal.set_enabled(false)
 		$Traces.modulate = Globals.TRACE_DEACTIVATED
 	
+	rec_mul.check_error()
 	rec_mul.inputs.clear()
