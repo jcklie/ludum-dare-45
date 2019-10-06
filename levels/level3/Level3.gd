cd93dc49bfa5e4d8e4fdcb64907da68a7f603e6e
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-10-06 17:30:57 +0200

    Level 3 goal announces win condition via audio.

--- levels/level3/Level3.gd
+++ levels/level3/Level3.gd
@@ -3,7 +3,23 @@ extends Node2D
 onready var rec_mul = $"receivers/ReceiverMulti"
 onready var goal = $Goal
 
-func _process(delta):
+var win_condition = {69: true, 76: true}
+onready var Note = preload("res://logic/note.gd").Note
+
+onready var sfx_howl = preload("res://audio/sfx/howl01_A.ogg")
+
+func _ready():
+	# add howl for each note required for activating the goal	
+	var base_volume = linear2db(1.0/win_condition.size())
+	for midi_note_number in win_condition.keys():
+		var n = Note.new(midi_note_number)
+		var asp = PlayerAttenuatingAudioStreamPlayer.new()
+		asp.set_params(base_volume, 128, 1)
+		asp.stream = sfx_howl
+		asp.pitch_scale = n.well_tempered_factor_from_concert_pitch()
+		goal.add_child(asp)
+
+func _process(delta):	
 	var success = rec_mul.inputs.size() == 2 and 69 in rec_mul.inputs and 76 in rec_mul.inputs
 	if success:
 		goal.set_enabled(true)
