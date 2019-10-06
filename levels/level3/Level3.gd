a8c6319e463733a23508f5cc4db73ce973e933af
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-10-06 15:25:25 +0200

    Dummy for level 3.

--- levels/level3/Level3.gd
+++ levels/level3/Level3.gd
@@ -1,13 +1,7 @@
 extends Node2D
 
 onready var drain = $"drains/Drain"
-onready var door = $Door
 
-func _process(delta):
-
+func _process(delta):	
 	if 69 in drain.inputs and 76 in drain.inputs:
-		door.open()
-	else:
-		door.close()
-		
-	drain.inputs.clear()
+		pass
