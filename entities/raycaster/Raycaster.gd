d15cf49d14f77763c52b536391b25fec9112f50a
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2019-10-06 15:11:28 +0200

    Restructure levels 1, 2. Fix player blocking beams (cosmetic).

--- entities/raycaster/Raycaster.gd
+++ entities/raycaster/Raycaster.gd
@@ -8,6 +8,6 @@ func raycast(position, rotation):
 	
 	var world_phys_state = get_world_2d().direct_space_state
 	var ray_end_pos = position + direction * MAX_RAYCAST_LENGTH
-	var hit = world_phys_state.intersect_ray(global_position, ray_end_pos, [self], 5)	# impassable terrain, emitter, drain
+	var hit = world_phys_state.intersect_ray(global_position, ray_end_pos, [self], 1)	# impassable terrain, emitter, drain
 	
 	return hit
